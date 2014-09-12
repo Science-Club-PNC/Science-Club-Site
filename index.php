2315db763db24892c09e061e920630db59556240
Dani van Enk <danivenk@gmail.com>
2014-09-12 15:57:17 +0200

    Upload Files

---
+++ index.php
@@ -0,0 +1,60 @@
+<!Doctype HTML>
+<html>
+<head>
+	<title>
+		Home
+	</title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" href="css/css.css">
+	<link rel="shortcut icon" type="image/png" href="img/alt icon8.png">
+</head>
+<body>
+	<div class="container">
+		<!--Navigation Include-->
+		<?php
+
+		include 'nav.php';
+
+		?>
+		<!--Website content-->
+		<div class="content">
+			<h1>Science Club</h1>
+			<p>Bij Science Club kan je Informatica, Natuurkunde en Scheikunde gebruiken op een interessante manier. Je kan er Natuurkunde experimenten doen, scheikunde experimenten doen, programmeren en websites maken. Iedereen leert van elkaar en als je vragen hebt vraag het aan je medeleerlingen en de leraren die er zijn.</p>
+
+			<p>Pieternieuwland College lokaal P117<br>
+			vrijdag 7e t/m 9e uur (14.20 – 17.00)<br>
+			H. Rejaibi (HBRI), S. Dinkgreve (PDIN)</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a cursus urna, quis molestie elit. Etiam fermentum nunc quis nulla aliquet sagittis. Aenean euismod, lorem at congue imperdiet, ligula augue consequat diam, vitae porta turpis risus non mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla tincidunt lectus eu libero tempor, pellentesque facilisis velit venenatis. Integer a metus pulvinar, sodales velit a, porttitor sem. Fusce sit amet maximus sem. Etiam ac tempor diam. Maecenas finibus velit maximus sodales iaculis. Ut porta convallis ligula, et laoreet nisl molestie sit amet. Quisque vestibulum sem non congue vestibulum. Proin ullamcorper ligula ac enim rhoncus auctor id a ipsum. Donec nec sollicitudin elit. Nam eu varius dui. Donec sed hendrerit elit. Donec vestibulum eu lorem congue finibus.</p>
+
+			<p>Maecenas vitae feugiat lectus. Etiam neque lectus, finibus vel ante nec, interdum accumsan massa. Suspendisse vitae posuere lacus, sed facilisis risus. Proin auctor ac lorem a tempor. Fusce commodo gravida dapibus. Nam interdum, erat ut venenatis gravida, diam massa posuere justo, a faucibus ligula tortor in dui. Fusce porttitor in erat id malesuada. Nunc erat justo, volutpat ut nunc sed, placerat dignissim nulla. Pellentesque eu mauris in mi pulvinar cursus.</p>
+
+			<p>Ut volutpat consequat nulla, sed tincidunt urna sagittis sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare, sem non elementum ultrices, lorem magna sodales nulla, et egestas risus nunc in justo. Phasellus sodales interdum placerat. Nullam ut iaculis libero. Proin eu velit sem. Aenean in vestibulum magna, id porta magna. Sed pulvinar sem non massa mattis, nec egestas metus consequat. Ut vitae vehicula justo. Donec finibus quam at rhoncus molestie. Maecenas in vehicula leo. Pellentesque eu magna lectus. Vivamus eget augue dapibus, scelerisque eros a, posuere leo. Quisque ullamcorper eget risus eu interdum. Donec at metus in nisl maximus sagittis at et lorem.</p>
+
+			<p>Ut rutrum a ligula non sagittis. Vivamus rhoncus felis at ex scelerisque bibendum. Nulla nec pellentesque dolor, sed dignissim velit. Cras nec posuere ligula. Etiam neque lacus, rutrum vitae turpis pharetra, posuere convallis ipsum. Praesent egestas non tellus tempus mattis. Nunc imperdiet sed magna tristique consectetur.</p>
+
+			<p>Duis et faucibus arcu, at vulputate mi. Morbi porttitor pretium placerat. Aenean id augue a erat placerat tempor ut at quam. Nunc vulputate molestie lorem, at egestas elit gravida vel. Maecenas at velit tortor. Nullam scelerisque nulla ac mollis consectetur. Integer mauris eros, pulvinar tempus lacinia sed, pretium eu urna. Mauris aliquam est ut nisl congue, non venenatis mi posuere. In laoreet eros nulla. Maecenas sed quam at eros placerat consequat. Phasellus id lobortis magna. Maecenas vitae vestibulum magna, at pellentesque eros. Suspendisse a scelerisque urna. Phasellus gravida augue a libero lobortis feugiat. In hac habitasse platea dictumst.</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a cursus urna, quis molestie elit. Etiam fermentum nunc quis nulla aliquet sagittis. Aenean euismod, lorem at congue imperdiet, ligula augue consequat diam, vitae porta turpis risus non mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla tincidunt lectus eu libero tempor, pellentesque facilisis velit venenatis. Integer a metus pulvinar, sodales velit a, porttitor sem. Fusce sit amet maximus sem. Etiam ac tempor diam. Maecenas finibus velit maximus sodales iaculis. Ut porta convallis ligula, et laoreet nisl molestie sit amet. Quisque vestibulum sem non congue vestibulum. Proin ullamcorper ligula ac enim rhoncus auctor id a ipsum. Donec nec sollicitudin elit. Nam eu varius dui. Donec sed hendrerit elit. Donec vestibulum eu lorem congue finibus.</p>
+
+			<p>Maecenas vitae feugiat lectus. Etiam neque lectus, finibus vel ante nec, interdum accumsan massa. Suspendisse vitae posuere lacus, sed facilisis risus. Proin auctor ac lorem a tempor. Fusce commodo gravida dapibus. Nam interdum, erat ut venenatis gravida, diam massa posuere justo, a faucibus ligula tortor in dui. Fusce porttitor in erat id malesuada. Nunc erat justo, volutpat ut nunc sed, placerat dignissim nulla. Pellentesque eu mauris in mi pulvinar cursus.</p>
+
+			<p>Ut volutpat consequat nulla, sed tincidunt urna sagittis sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare, sem non elementum ultrices, lorem magna sodales nulla, et egestas risus nunc in justo. Phasellus sodales interdum placerat. Nullam ut iaculis libero. Proin eu velit sem. Aenean in vestibulum magna, id porta magna. Sed pulvinar sem non massa mattis, nec egestas metus consequat. Ut vitae vehicula justo. Donec finibus quam at rhoncus molestie. Maecenas in vehicula leo. Pellentesque eu magna lectus. Vivamus eget augue dapibus, scelerisque eros a, posuere leo. Quisque ullamcorper eget risus eu interdum. Donec at metus in nisl maximus sagittis at et lorem.</p>
+
+			<p>Ut rutrum a ligula non sagittis. Vivamus rhoncus felis at ex scelerisque bibendum. Nulla nec pellentesque dolor, sed dignissim velit. Cras nec posuere ligula. Etiam neque lacus, rutrum vitae turpis pharetra, posuere convallis ipsum. Praesent egestas non tellus tempus mattis. Nunc imperdiet sed magna tristique consectetur.</p>
+
+			<p>Duis et faucibus arcu, at vulputate mi. Morbi porttitor pretium placerat. Aenean id augue a erat placerat tempor ut at quam. Nunc vulputate molestie lorem, at egestas elit gravida vel. Maecenas at velit tortor. Nullam scelerisque nulla ac mollis consectetur. Integer mauris eros, pulvinar tempus lacinia sed, pretium eu urna. Mauris aliquam est ut nisl congue, non venenatis mi posuere. In laoreet eros nulla. Maecenas sed quam at eros placerat consequat. Phasellus id lobortis magna. Maecenas vitae vestibulum magna, at pellentesque eros. Suspendisse a scelerisque urna. Phasellus gravida augue a libero lobortis feugiat. In hac habitasse platea dictumst.</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a cursus urna, quis molestie elit. Etiam fermentum nunc quis nulla aliquet sagittis. Aenean euismod, lorem at congue imperdiet, ligula augue consequat diam, vitae porta turpis risus non mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla tincidunt lectus eu libero tempor, pellentesque facilisis velit venenatis. Integer a metus pulvinar, sodales velit a, porttitor sem. Fusce sit amet maximus sem. Etiam ac tempor diam. Maecenas finibus velit maximus sodales iaculis. Ut porta convallis ligula, et laoreet nisl molestie sit amet. Quisque vestibulum sem non congue vestibulum. Proin ullamcorper ligula ac enim rhoncus auctor id a ipsum. Donec nec sollicitudin elit. Nam eu varius dui. Donec sed hendrerit elit. Donec vestibulum eu lorem congue finibus.</p>
+
+			<p>Maecenas vitae feugiat lectus. Etiam neque lectus, finibus vel ante nec, interdum accumsan massa. Suspendisse vitae posuere lacus, sed facilisis risus. Proin auctor ac lorem a tempor. Fusce commodo gravida dapibus. Nam interdum, erat ut venenatis gravida, diam massa posuere justo, a faucibus ligula tortor in dui. Fusce porttitor in erat id malesuada. Nunc erat justo, volutpat ut nunc sed, placerat dignissim nulla. Pellentesque eu mauris in mi pulvinar cursus.</p>
+
+			<p>Ut volutpat consequat nulla, sed tincidunt urna sagittis sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare, sem non elementum ultrices, lorem magna sodales nulla, et egestas risus nunc in justo. Phasellus sodales interdum placerat. Nullam ut iaculis libero. Proin eu velit sem. Aenean in vestibulum magna, id porta magna. Sed pulvinar sem non massa mattis, nec egestas metus consequat. Ut vitae vehicula justo. Donec finibus quam at rhoncus molestie. Maecenas in vehicula leo. Pellentesque eu magna lectus. Vivamus eget augue dapibus, scelerisque eros a, posuere leo. Quisque ullamcorper eget risus eu interdum. Donec at metus in nisl maximus sagittis at et lorem.</p>
+
+			<p>Ut rutrum a ligula non sagittis. Vivamus rhoncus felis at ex scelerisque bibendum. Nulla nec pellentesque dolor, sed dignissim velit. Cras nec posuere ligula. Etiam neque lacus, rutrum vitae turpis pharetra, posuere convallis ipsum. Praesent egestas non tellus tempus mattis. Nunc imperdiet sed magna tristique consectetur.</p>
+
+			<p>Duis et faucibus arcu, at vulputate mi. Morbi porttitor pretium placerat. Aenean id augue a erat placerat tempor ut at quam. Nunc vulputate molestie lorem, at egestas elit gravida vel. Maecenas at velit tortor. Nullam scelerisque nulla ac mollis consectetur. Integer mauris eros, pulvinar tempus lacinia sed, pretium eu urna. Mauris aliquam est ut nisl congue, non venenatis mi posuere. In laoreet eros nulla. Maecenas sed quam at eros placerat consequat. Phasellus id lobortis magna. Maecenas vitae vestibulum magna, at pellentesque eros. Suspendisse a scelerisque urna. Phasellus gravida augue a libero lobortis feugiat. In hac habitasse platea dictumst.</p>
+		</div>
+	</div>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,13 @@
+<?php
+
+$site = "";
+
+?>
+
+<!--Navigation Bar-->
+	<ul id="nav" class="nav">
+		<li><a href="<?php echo $site?>index.php">Home</a></li>
+		<li><a href="<?php echo $site?>index.php">Iets</a></li>
+		<li><a href="<?php echo $site?>index.php">Anders</a></li>
+		<li><a href="<?php echo $site?>index.php">Deze</a></li>
+	</ul><br><br><br>
