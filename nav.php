049af114dac2eea51f36767731aacdb23e41f480
Dani van Enk <danivenk@gmail.com>
2014-09-19 16:08:09 +0200

    Linking nav

--- nav.php
+++ nav.php
@@ -7,7 +7,7 @@ $site = "";
 <!--Navigation Bar-->
 	<ul id="nav" class="nav">
 		<li><a href="<?php echo $site?>index.php">Home</a></li>
-		<li><a href="<?php echo $site?>index.php">Projecten</a></li>
+		<li><a href="<?php echo $site?>projecten/index.php">Projecten</a></li>
 		<li><a href="<?php echo $site?>index.php">Anders</a></li>
 		<li><a href="<?php echo $site?>index.php">Deze</a></li>
 	</ul><br><br><br>
