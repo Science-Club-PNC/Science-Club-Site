48bb1e211957209462ce70dac52a589067f9ec1f
Mátyás Tóth <azhnarton@gmail.com>
2014-09-19 16:48:13 +0200

    Nav change
    Adjusted position, rotation, etc. Tok Tok

--- nav.php
+++ nav.php
@@ -5,9 +5,9 @@ $site = "";
 ?>
 
 <!--Navigation Bar-->
-	<ul id="nav" class="nav">
-		<li><a href="<?php echo $site?>index.php">Home</a></li>
-		<li><a href="<?php echo $site?>index.php">Projecten</a></li>
-		<li><a href="<?php echo $site?>index.php">Anders</a></li>
-		<li><a href="<?php echo $site?>index.php">Deze</a></li>
-	</ul><br><br><br>
+	<div id="nav" class="nav">
+		<a href="<?php echo $site?>index.php">Home</a>
+		<a href="<?php echo $site?>index.php">Projecten</a>
+		<a href="<?php echo $site?>index.php">Anders</a>
+		<a href="<?php echo $site?>index.php">Deze</a>
+	</div><br><br><br>
