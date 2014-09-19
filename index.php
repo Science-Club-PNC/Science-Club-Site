fd0a127a7fe3dca8a8457d5b91ffb1e980c5af37
buildersteven4 <buildersteven4@gmail.com>
2014-09-19 22:41:10 +0200

    Fixed background
    I found some bugs in the background CSS while cleaning it. So I have
    fixed it with adding a div into the body and placing the background on
    that. I know it isn't a clean fix, but I do believe it is the cleanest
    possible fix. I've tried over 20 possible way to fix it without use of a
    helper html object, but none of them were successful... (CSS sucks)

--- index.php
+++ index.php
@@ -9,22 +9,24 @@
 	<link rel="shortcut icon" type="image/png" href="img/icon.png">
 </head>
 <body>
-	<div class="container">
-		<!--Navigation Include-->
-		<?php
+	<div>
+		<div class="container">
+			<!--Navigation Include-->
+			<?php
 
-		include 'nav.php';
+			include 'nav.php';
 
-		?>
-		<!--Website content-->
-		<div class="content">
-			<h1>Science Club</h1>
-			<p>Bij Science Club kan je Informatica, Natuurkunde en Scheikunde gebruiken op een interessante manier. Je kan er Natuurkunde experimenten doen, scheikunde experimenten doen, programmeren en websites maken. Iedereen leert van elkaar en als je vragen hebt vraag het aan je medeleerlingen en de leraren die er zijn.</p>
+			?>
+			<!--Website content-->
+			<div class="content">
+				<h1>Science Club</h1>
+				<p>Bij Science Club kan je Informatica, Natuurkunde en Scheikunde gebruiken op een interessante manier. Je kan er Natuurkunde experimenten doen, scheikunde experimenten doen, programmeren en websites maken. Iedereen leert van elkaar en als je vragen hebt vraag het aan je medeleerlingen en de leraren die er zijn.</p>
 
-			<p>Pieternieuwland College lokaal P117<br>
-			vrijdag 7e t/m 9e uur (14.20 – 17.00)<br>
-			H. Rejaibi (HBRI), S. Dinkgreve (PDIN)</p>
+				<p>Pieternieuwland College lokaal P117<br>
+				vrijdag 7e t/m 9e uur (14.20 – 17.00)<br>
+				H. Rejaibi (HBRI), S. Dinkgreve (PDIN)</p>
+			</div>
 		</div>
-	</div>
+	<div>
 </body>
 </html>
