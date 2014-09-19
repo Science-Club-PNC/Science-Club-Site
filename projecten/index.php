f3e92d8e30920d47f8f172c3cba1839c80a8b5df
Dani van Enk <danivenk@gmail.com>
2014-09-19 16:41:33 +0200

    border and shit

--- projecten/index.php
+++ projecten/index.php
@@ -24,33 +24,22 @@
 
 			<p>Het plan moet bestaan uit de volgende drie onderdelen:</p>
 
-			<p>1.	De titel</p>
-
-			<p>Bedenk een goed leesbare titel waaruit meteen duidelijk wordt waar het onderzoek over gaat</p>
-
-			<p>2.	De onderzoeksvraag</p>
-
-			<p>Schrijf kort op wat het doel van de proef is. Doe dit aan de hand van een onderzoeksvraag. Zorg dat deze vraag erg precies geformuleerd is.</p>
-
-			<p>3.	Het werkplan</p>
-
-			<p>In dit onderdeel geef je een lijst met de spullen die gebruikt zijn, maak je een schematische tekening van de opstelling die je gebruikt hebt en schrijf je een korte beschrijving van hoe je het experiment hebt uitgevoerd.</p>
+			<div class="box"><h3>1. De titel</h3>
+			Bedenk een goed leesbare titel waaruit meteen duidelijk wordt waar het onderzoek over gaat
+			<h3>2. De onderzoeksvraag</h3>
+			Schrijf kort op wat het doel van de proef is. Doe dit aan de hand van een onderzoeksvraag. Zorg dat deze vraag erg precies geformuleerd is.
+			<h3>3. Het werkplan</h3>
+			In dit onderdeel geef je een lijst met de spullen die gebruikt zijn, maak je een schematische tekening van de opstelling die je gebruikt hebt en schrijf je een korte beschrijving van hoe je het experiment hebt uitgevoerd.</div>
 
 			<p>Tijdens het onderzoek leg je de volgende gegevens vast:</p>
 
-			<p>4.	Onderzoeksresultaten</p>
-
-			<p>Hierin vermeld je wat je hebt waargenomen of gemeten. Dit kan met behulp van tekst, foto's, video's en meestal worden hier ook tabellen of grafieken verwacht</p>
-
-			<p>5.	De conclusie</p>
-
-			<p>Hierin vermeld je het antwoord op de onderzoeksvraag op basis van de resultaten die je gevonden hebt. Als een experiment niet gelukt is, dan vertel je hier waarin dit niet gelukt is en wat je de volgende keer anders zou doen.</p>
-
-			<p>6.	De discussie</p>
-
-			<p>In dit stuk noteer je mogelijke onzekerheden in de resultaten die je gevonden hebt. Het is belangrijk om hier volledig eerlijk te zijn. Ook schrijf je hier op wat je de volgende keer anders zou doen, schrijf je op of er nog vragen onbeantwoord zijn gebleven of dat het experiment weer nieuwe vragen op heeft geroepen die nog niet beantwoord zijn.</p>
-
-			<p>7.	Vermeld de bronnen</p>
+			<div class="box"><h3>4. Onderzoeksresultaten</h3>
+			Hierin vermeld je wat je hebt waargenomen of gemeten. Dit kan met behulp van tekst, foto's, video's en meestal worden hier ook tabellen of grafieken verwacht.
+			<h3>5. De conclusie</h3>
+			Hierin vermeld je het antwoord op de onderzoeksvraag op basis van de resultaten die je gevonden hebt. Als een experiment niet gelukt is, dan vertel je hier waarin dit niet gelukt is en wat je de volgende keer anders zou doen.
+			<h3>6. De discussie</h3>
+			In dit stuk noteer je mogelijke onzekerheden in de resultaten die je gevonden hebt. Het is belangrijk om hier volledig eerlijk te zijn. Ook schrijf je hier op wat je de volgende keer anders zou doen, schrijf je op of er nog vragen onbeantwoord zijn gebleven of dat het experiment weer nieuwe vragen op heeft geroepen die nog niet beantwoord zijn.
+			<h3>7. Vermeld de bronnen</h3></div>
 
 			<p>De resultaten (inclusief eventuele fimlpjes) worden op de website gepubliceerd!</p>
 		</div>
