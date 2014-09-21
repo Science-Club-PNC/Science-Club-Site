dbe3e0ab3c4da70a6be3ec9a5f495916bbb63eda
Dani van Enk <danivenk@gmail.com>
2014-09-21 17:53:49 +0200

    Conflict Fixed

--- index.php
+++ index.php
@@ -9,40 +9,26 @@
 	<link rel="shortcut icon" type="image/png" href="img/icon.png">
 </head>
 <body>
-<<<<<<< HEAD
-	<div class="container">
-		<!--Navigation Bar-->
-		<div id="nav" class="nav">
-			<a href="<?php echo $site?>index.php">Home</a>
-			<a href="<?php echo $site?>index.php">Projecten</a>
-			<a href="<?php echo $site?>index.php">Anders</a>
-			<a href="<?php echo $site?>index.php">Deze</a>
-		</div><br><br><br>
-	
-		<!--Website content-->
-		<div class="content">
-			<h1>Science Club</h1>
-			<p>Bij Science Club kan je Informatica, Natuurkunde en Scheikunde gebruiken op een interessante manier. Je kan er Natuurkunde experimenten doen, scheikunde experimenten doen, programmeren en websites maken. Iedereen leert van elkaar en als je vragen hebt vraag het aan je medeleerlingen en de leraren die er zijn.</p>
-=======
 	<div>
-		<div class="container">
-			<!--Navigation Include-->
-			<?php
-
-			include 'nav.php';
-
-			?>
+	 <div class="container">
+		 	<!--Navigation Bar-->
+			<div id="nav" class="nav">
+				<a href="<?php echo $site?>index.php">Home</a>
+				<a href="<?php echo $site?>index.php">Projecten</a>
+				<a href="<?php echo $site?>index.php">Anders</a>
+				<a href="<?php echo $site?>index.php">Deze</a>
+			</div><br><br><br>
+	
 			<!--Website content-->
 			<div class="content">
 				<h1>Science Club</h1>
 				<p>Bij Science Club kan je Informatica, Natuurkunde en Scheikunde gebruiken op een interessante manier. Je kan er Natuurkunde experimenten doen, scheikunde experimenten doen, programmeren en websites maken. Iedereen leert van elkaar en als je vragen hebt vraag het aan je medeleerlingen en de leraren die er zijn.</p>
->>>>>>> c39318159aeac2ffbb739a185096dc9457cd547b
 
 				<p>Pieternieuwland College lokaal P117<br>
 				vrijdag 7e t/m 9e uur (14.20 – 17.00)<br>
 				H. Rejaibi (HBRI), S. Dinkgreve (PDIN)</p>
 			</div>
 		</div>
-	<div>
+	</div>
 </body>
 </html>
