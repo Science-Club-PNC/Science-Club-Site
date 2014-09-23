cc91790189805db5c42569b2bb32ad9342e9afd4
Dani van Enk <danivenk@gmail.com>
2014-09-23 20:04:18 +0200

    index.php recovery
    Steven and I agreed monday to use the index.php, so here it's back.
    Someone had deleted it.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!Doctype HTML>
+<html>
+<head>
+	<title>
+		Home
+	</title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" href="css/css.css">
+	<link rel="shortcut icon" type="image/png" href="img/icon.png">
+</head>
+<body>
+	<div>
+		<div class="container">
+		 	<!--Navigation Bar-->
+			<div id="nav" class="nav">
+				<a href="index.php">Home</a>
+				<a href="projecten/index.php">Projecten</a>
+				<a href="index.php">Anders</a>
+				<a href="index.php">Deze</a>
+			</div>
+	
+			<!--Website content-->
+			<div class="content">
+				<h1>Science Club</h1>
+				<p>Bij Science Club kan je Informatica, Natuurkunde en Scheikunde gebruiken op een interessante manier. Je kan er Natuurkunde experimenten doen, scheikunde experimenten doen, programmeren en websites maken. Iedereen leert van elkaar en als je vragen hebt vraag het aan je medeleerlingen en de leraren die er zijn.</p>
+
+				<p>Pieternieuwland College lokaal P117<br>
+				vrijdag 7e t/m 9e uur (14.20 – 17.00)<br>
+				H. Rejaibi (HBRI), S. Dinkgreve (PDIN)</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
