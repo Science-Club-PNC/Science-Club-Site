cfcd7480271a568c2a08e514cc82bb19853ea753
buildersteven4 <buildersteven4@gmail.com>
2014-09-25 15:04:17 +0200

    contentacle content change
    Sleek... :3

--- pages/home.php
+++ pages/home.php
@@ -2,5 +2,5 @@
 <p>Bij Science Club kan je Informatica, Natuurkunde en Scheikunde gebruiken op een interessante manier. Je kan er Natuurkunde experimenten doen, scheikunde experimenten doen, programmeren en websites maken. Iedereen leert van elkaar en als je vragen hebt vraag het aan je medeleerlingen en de leraren die er zijn.</p>
 
 <p>Pieternieuwland College lokaal P117<br>
-vrijdag 7e t/m 9e uur (14.20 tot ong. 17.00, maar je kan langer blijven als je echt wil.)<br>
+Elke vrijdag vanaf het 7e uur<br>
 H. Rejaibi (HBRI), S. Dinkgreve (PDIN)</p>
