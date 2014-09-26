a4dcd70f7ea3e24de9fdba5a7d2d7ba580965300
Dani van Enk <danivenk@gmail.com>
2014-09-26 12:41:31 +0200

    projecten page content in php

--- projecten/index.php
+++ projecten/index.php
@@ -21,30 +21,23 @@
 
 			<!--Website content-->
 			<div class="content">
-				<h1>Een werkplan indienen</h1>
-				
-				<p>Wil je een experiment uitvoeren op de vrijdagmiddag bij de science club? Dan moet je eerst een werkplan indienen, zodat we de materialen bij elkaar kunnen zoeken of eventueel kunnen bestellen.</p>
+				<?php
 
-				<p>Het plan moet bestaan uit de volgende drie onderdelen:</p>
+				if(empty($_GET['page']) || !isset($_GET['page'])) {
+					$page = "projecten";
+				} else {
+					$page = $_GET['page'];
+				}
 
-				<div class="box"><h3>1. De titel</h3>
-				Bedenk een goed leesbare titel waaruit meteen duidelijk wordt waar het onderzoek over gaat
-				<h3>2. De onderzoeksvraag</h3>
-				Schrijf kort op wat het doel van de proef is. Doe dit aan de hand van een onderzoeksvraag. Zorg dat deze vraag erg precies geformuleerd is.
-				<h3>3. Het werkplan</h3>
-				In dit onderdeel geef je een lijst met de spullen die gebruikt zijn, maak je een schematische tekening van de opstelling die je gebruikt hebt en schrijf je een korte beschrijving van hoe je het experiment hebt uitgevoerd.</div>
+				$path = "../pages/" . $page . ".php";
 
-				<p>Tijdens het onderzoek leg je de volgende gegevens vast:</p>
+				if(!file_exists($path)) {
+					$path = "pages/404.php";
+				}
 
-				<div class="box"><h3>4. Onderzoeksresultaten</h3>
-				Hierin vermeld je wat je hebt waargenomen of gemeten. Dit kan met behulp van tekst, foto's, video's en meestal worden hier ook tabellen of grafieken verwacht.
-				<h3>5. De conclusie</h3>
-				Hierin vermeld je het antwoord op de onderzoeksvraag op basis van de resultaten die je gevonden hebt. Als een experiment niet gelukt is, dan vertel je hier waarin dit niet gelukt is en wat je de volgende keer anders zou doen.
-				<h3>6. De discussie</h3>
-				In dit stuk noteer je mogelijke onzekerheden in de resultaten die je gevonden hebt. Het is belangrijk om hier volledig eerlijk te zijn. Ook schrijf je hier op wat je de volgende keer anders zou doen, schrijf je op of er nog vragen onbeantwoord zijn gebleven of dat het experiment weer nieuwe vragen op heeft geroepen die nog niet beantwoord zijn.
-				<h3>7. Vermeld de bronnen</h3></div>
+				include_once($path);
 
-				<p>De resultaten (inclusief eventuele fimlpjes) worden op de website gepubliceerd!</p>
+                ?>
 			</div>
 		</div>
 	</div>
