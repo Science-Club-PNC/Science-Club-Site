11943c9fea2810214fc0d1f9923af582b77c882d
Dany Sluijk <danysluyk@live.nl>
2014-09-26 17:01:56 +0200

    Use a freaking structure!

--- pages/projecten.php
+++ pages/projecten.php
@@ -1,24 +1,28 @@
-				<h1>Een werkplan indienen</h1>
-				
-				<p>Wil je een experiment uitvoeren op de vrijdagmiddag bij de science club? Dan moet je eerst een werkplan indienen, zodat we de materialen bij elkaar kunnen zoeken of eventueel kunnen bestellen.</p>
+<h1>Een werkplan indienen</h1>
+	
+<p>Wil je een experiment uitvoeren op de vrijdagmiddag bij de science club? Dan moet je eerst een werkplan indienen, zodat we de materialen bij elkaar kunnen zoeken of eventueel kunnen bestellen.</p>
 
-				<p>Het plan moet bestaan uit de volgende drie onderdelen:</p>
+<p>Het plan moet bestaan uit de volgende drie onderdelen:</p>
 
-				<div class="box"><h3>1. De titel</h3>
-				<p>Bedenk een goed leesbare titel waaruit meteen duidelijk wordt waar het onderzoek over gaat</p>
-				<h3>2. De onderzoeksvraag</h3>
-				<p>Schrijf kort op wat het doel van de proef is. Doe dit aan de hand van een onderzoeksvraag. Zorg dat deze vraag erg precies geformuleerd is.</p>
-				<h3>3. Het werkplan</h3>
-				<p>In dit onderdeel geef je een lijst met de spullen die gebruikt zijn, maak je een schematische tekening van de opstelling die je gebruikt hebt en schrijf je een korte beschrijving van hoe je het experiment hebt uitgevoerd.</p></div>
+<div class="box">
+    <h3>1. De titel</h3>
+	<p>Bedenk een goed leesbare titel waaruit meteen duidelijk wordt waar het onderzoek over gaat</p>
+	<h3>2. De onderzoeksvraag</h3>
+	<p>Schrijf kort op wat het doel van de proef is. Doe dit aan de hand van een onderzoeksvraag. Zorg dat deze vraag erg precies geformuleerd is.</p>
+	<h3>3. Het werkplan</h3>
+	<p>In dit onderdeel geef je een lijst met de spullen die gebruikt zijn, maak je een schematische tekening van de opstelling die je gebruikt hebt en schrijf je een korte beschrijving van hoe je het experiment hebt uitgevoerd.</p>
+</div>
 
-				<p>Tijdens het onderzoek leg je de volgende gegevens vast:</p>
+<p>Tijdens het onderzoek leg je de volgende gegevens vast:</p>
 
-				<div class="box"><h3>4. Onderzoeksresultaten</h3>
-				<p>Hierin vermeld je wat je hebt waargenomen of gemeten. Dit kan met behulp van tekst, foto's, video's en meestal worden hier ook tabellen of grafieken verwacht.</p>
-				<h3>5. De conclusie</h3>
-				<p>Hierin vermeld je het antwoord op de onderzoeksvraag op basis van de resultaten die je gevonden hebt. Als een experiment niet gelukt is, dan vertel je hier waarin dit niet gelukt is en wat je de volgende keer anders zou doen.</p>
-				<h3>6. De discussie</h3>
-				<p>In dit stuk noteer je mogelijke onzekerheden in de resultaten die je gevonden hebt. Het is belangrijk om hier volledig eerlijk te zijn. Ook schrijf je hier op wat je de volgende keer anders zou doen, schrijf je op of er nog vragen onbeantwoord zijn gebleven of dat het experiment weer nieuwe vragen op heeft geroepen die nog niet beantwoord zijn.</p>
-				<h3>7. Vermeld de bronnen</h3></div>
+<div class="box">
+	<h3>4. Onderzoeksresultaten</h3>
+	<p>Hierin vermeld je wat je hebt waargenomen of gemeten. Dit kan met behulp van tekst, foto's, video's en meestal worden hier ook tabellen of grafieken verwacht.</p>
+	<h3>5. De conclusie</h3>
+	<p>Hierin vermeld je het antwoord op de onderzoeksvraag op basis van de resultaten die je gevonden hebt. Als een experiment niet gelukt is, dan vertel je hier waarin dit niet gelukt is en wat je de volgende keer anders zou doen.</p>
+	<h3>6. De discussie</h3>
+	<p>In dit stuk noteer je mogelijke onzekerheden in de resultaten die je gevonden hebt. Het is belangrijk om hier volledig eerlijk te zijn. Ook schrijf je hier op wat je de volgende keer anders zou doen, schrijf je op of er nog vragen onbeantwoord zijn gebleven of dat het experiment weer nieuwe vragen op heeft geroepen die nog niet beantwoord zijn.</p>
+	<h3>7. Vermeld de bronnen</h3>
+</div>
 
-				<p>De resultaten (inclusief eventuele fimlpjes) worden op de website gepubliceerd!</p>
+<p>De resultaten (inclusief eventuele fimlpjes) worden op de website gepubliceerd!</p>
